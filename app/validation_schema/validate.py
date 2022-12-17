ee1e42487ceb272bfbdd76cae14dcf15235c6c90
João Lucas Flauzino Cassiano <joaolucas.apoio@gmail.com>
2022-12-17 16:55:29 -0300

    Ajustando validação de entrada na api

--- app/validation_schema/validate.py
+++ app/validation_schema/validate.py
@@ -5,16 +5,18 @@ from typing import List
 class Register(BaseModel):
     category_id: int
     category_name: str
-    subcategory_id: int
-    subcategory_name: str
+    budget_type: str
     budget: float
-
-
-# Schema to a List of Register
-class ListRegister(BaseModel):
-    registers: List[Register]
 
 
 # Schema to Delete
 class Delete(BaseModel):
     category_id: int
+
+
+class Category(BaseModel):
+    category_id: int
+
+
+class ListCategory(BaseModel):
+    categories_ids: List[Category]
