8826b47c1c474f68f732824600c0e985aa582c29
João Lucas Flauzino Cassiano <joaolucas.apoio@gmail.com>
2022-12-19 22:14:38 -0300

    Criando rotas para a tabela de transação

--- app/api.py
+++ app/api.py
@@ -6,10 +6,18 @@ from database import register_engine
 from fastapi.encoders import jsonable_encoder
 from typing import List, Union
 
-from validation_schema.validate import Register, Delete
+from validation_schema.validate import (
+    Register,
+    Delete,
+    RegisterTransaction,
+    RegisterUpdateTransaction,
+    DeleteTransaction,
+)
 
 from database.operations import DataBaseOperations
-from database.register_engine import DimensionFinanceTable
+from database.register_engine import DimensionFinanceTable, FactTransactionFinance
+
+from datetime import datetime
 
 
 register_engine.create_database_engine()
@@ -23,14 +31,14 @@ def root():
     return {"message": "It is working!"}
 
 
-@app.get("/budget/", status_code=status.HTTP_200_OK)
+@app.get("/dimension/budget/", status_code=status.HTTP_200_OK)
 def read_budget(items: Union[List[int], None] = Query(default=[1])):
     register = DataBaseOperations()
     rsp = register.get_instance(items, DimensionFinanceTable)
     return rsp
 
 
-@app.post("/budget/register", status_code=status.HTTP_201_CREATED)
+@app.post("/dimension/budget/register", status_code=status.HTTP_201_CREATED)
 def register(data: Register):
     data_transformed = jsonable_encoder(data)
 
@@ -46,7 +54,7 @@ def register(data: Register):
     return f"Register was created: {rsp}"
 
 
-@app.put("/budget/update", status_code=status.HTTP_201_CREATED)
+@app.put("/dimension/budget/update", status_code=status.HTTP_201_CREATED)
 def update(data: Register):
     data_transformed = jsonable_encoder(data)
     register = DataBaseOperations()
@@ -54,11 +62,55 @@ def update(data: Register):
     return rsp
 
 
-@app.delete("/budget/delete", status_code=status.HTTP_200_OK)
+@app.delete("/dimension/budget/delete", status_code=status.HTTP_200_OK)
 def delete(data: Delete):
     register = DataBaseOperations()
     data_transformed = jsonable_encoder(data)
     rsp = register.delete_instance(data_transformed, DimensionFinanceTable)
+    return f" These registers were deleted: {rsp}"
+
+
+@app.get("/transaction/budget/", status_code=status.HTTP_200_OK)
+def read_transaction(items: Union[List[int], None] = Query(default=[1])):
+    register = DataBaseOperations()
+    rsp = register.get_instance(items, FactTransactionFinance)
+    return rsp
+
+
+@app.post("/transaction/budget/register", status_code=status.HTTP_201_CREATED)
+def register(data: RegisterTransaction):
+    data_transformed = jsonable_encoder(data)
+
+    transaction_date = datetime.now()
+
+    transaction_finance_table_instance = FactTransactionFinance(
+        category_id=data_transformed.get("category_id"),
+        datetime_transaction=transaction_date,
+        amount=data_transformed.get("amount"),
+    )
+
+    register = DataBaseOperations()
+
+    rsp = register.create_instance(transaction_finance_table_instance)
+    return f"Register was created: {rsp}"
+
+
+@app.put("/transaction/budget/update", status_code=status.HTTP_201_CREATED)
+def update(data: RegisterUpdateTransaction):
+    data_transformed = jsonable_encoder(data)
+    data_transformed["datetime_transaction"] = datetime.strptime(
+        data_transformed["datetime_transaction"], "%Y-%m-%dT%H:%M:%S.%f"
+    )
+    register = DataBaseOperations()
+    rsp = register.update_instance(data_transformed, FactTransactionFinance)
+    return rsp
+
+
+@app.delete("/transaction/budget/delete", status_code=status.HTTP_200_OK)
+def delete(data: DeleteTransaction):
+    register = DataBaseOperations()
+    data_transformed = jsonable_encoder(data)
+    rsp = register.delete_instance(data_transformed, FactTransactionFinance)
     return f" These registers were deleted: {rsp}"
 
 
