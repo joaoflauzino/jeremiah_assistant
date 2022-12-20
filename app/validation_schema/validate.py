bc273cbd9ff9d92a9e501736cfeae63945677955
João Lucas Flauzino Cassiano <joaolucas.apoio@gmail.com>
2022-12-19 22:15:28 -0300

    Criando validações pelo pydantic para a tabela de transação

--- app/validation_schema/validate.py
+++ app/validation_schema/validate.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
 from typing import List
+from datetime import datetime
 
 
-# Schema to Register and Update
+# Schema to Register and Update in dimension table
 class Register(BaseModel):
     category_id: int
     category_name: str
@@ -10,14 +11,33 @@ class Register(BaseModel):
     budget: float
 
 
-# Schema to Delete
+# Schema to Delete in dimension table
 class Delete(BaseModel):
     category_id: int
 
 
+# Schema to Select in dimension table
 class Category(BaseModel):
     category_id: int
 
 
+# Schema to Select in dimension table
 class ListCategory(BaseModel):
     categories_ids: List[Category]
+
+
+# Schema to register a transcation in fact table
+class RegisterTransaction(BaseModel):
+    category_id: int
+    amount: float
+
+
+class RegisterUpdateTransaction(BaseModel):
+    transaction_id: int
+    category_id: int
+    amount: float
+    datetime_transaction: datetime
+
+
+class DeleteTransaction(BaseModel):
+    transaction_id: int
